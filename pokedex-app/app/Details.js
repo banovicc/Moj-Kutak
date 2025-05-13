e39ab1a925429641f3cb4d6e564c318e14176b49
banovicc <ibanov01@fesb.hr>
2025-05-13 14:36:59 +0200

    Dodane dodatne promjene za stil

--- pokedex-app/app/Details.js
+++ pokedex-app/app/Details.js
@@ -1,18 +1,35 @@
 import styles from "./Details.module.css";
 import Image from "next/image";
 
+function stripHtmlTags(str) {
+  if (!str) return "";
+  return str.replace(/<[^>]*>/g, "");
+}
+
 export default function Details({ shows, num }) {
   shows = shows.filter((show) => show.id === num);
+
+  const show = shows[0];
 
   return (
     <div className={styles.Main}>
       <div className={styles.FirstHalf}>
-        <h1 style={{ color: "black" }}>{shows[0].name}</h1>
+        <h1 style={{ color: "black", marginBottom: 5, textAlign: "center " }}>
+          {show.name}
+        </h1>
+        <div className={styles.Genres}>
+          {show.genres.map((genre) => (
+            <p className={styles.Genre}>{genre}</p>
+          ))}
+        </div>
+        <p>Trajanje {show.runtime} minuta</p>
+        <p>Opis:</p>
+        <p>{stripHtmlTags(show.summary)}</p>
       </div>
       <div className={styles.Image}>
         <Image
-          src={shows[0].image.medium}
-          alt={shows[0].name}
+          src={show.image.medium}
+          alt={show.name}
           width={320}
           height={450}
         />
